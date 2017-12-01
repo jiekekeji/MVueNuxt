09449888555daaf6ba4f84486742108bdff77b10
jiekekeji <jieke_rose@foxmail.com>
2017-12-01 17:34:47 +0800

    002-路由

---
+++ demo003/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      Logo
+    },
+    methods: {
+      async asyncData ({params}) {
+        let {data} = await axios.get(`https://my-api/posts/${params.id}`)
+        return {title: data.title}
+      }
+
+    }
+  }
+</script>
+
+<style scoped="">
+  .container {
+    height: 200px;
+    width: 100%;
+
+  }
+
+</style>
