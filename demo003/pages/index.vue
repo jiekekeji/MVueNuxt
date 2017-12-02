98f151b779130fdaae4e50f7e37ae5796a5bc1cf
jiekekeji <jieke_rose@foxmail.com>
2017-12-02 13:59:23 +0800

    003-异步数据

--- demo003/pages/index.vue
+++ demo003/pages/index.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="container">
-
+    <button @click="asyncData()">获取数据{{title}}</button>
   </div>
 </template>
 
 <script>
   import Logo from '~/components/Logo.vue'
-
+  import axios from 'axios'
   export default {
     components: {
       Logo
     },
+    async asyncData () {
+      let { data } = await axios.get(`http://localhost:8066/ydg/user/login`)
+      console.log("asyncData")
+      return { title: data }
+    },
     methods: {
-      async asyncData ({params}) {
-        let {data} = await axios.get(`https://my-api/posts/${params.id}`)
-        return {title: data.title}
-      }
 
     }
   }
